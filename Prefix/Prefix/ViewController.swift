7b3b57412b35c5febfc43bb6bfba3267e09f2c10
Cesar Ibarra <cfi2103@columbia.edu>
2016-09-11 17:06:02 -0400

    added prefix

--- Prefix/Prefix/ViewController.swift
+++ Prefix/Prefix/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Needle_In_Haystack
+//  Prefix
 //
 //  Created by Cesar Ibarra on 9/11/16.
 //  Copyright © 2016 Cesar Ibarra. All rights reserved.
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        receiveAPIInfo()
-        
+        getDictionary()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,17 +21,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    private func postAPIData(location: Int) {
-        let urlString = "http://challenge.code2040.org/api/haystack/validate"
+    private func postArray(array: NSMutableArray) {
+        let urlString = "http://challenge.code2040.org/api/prefix/validate"
         guard let url = URL(string: urlString) else {return}
         let session = URLSession.shared
         let request = NSMutableURLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
-        let jsonData: [String:String] = [
-            "token":"feb11791f036dc3f2ea5c1f39e41df63",
-            "needle":"\(location)"
+        print(array.description)
+        let jsonData: [String:AnyObject] = [
+            "array":array,
+            "token":"feb11791f036dc3f2ea5c1f39e41df63" as AnyObject
         ]
         do {
             request.httpBody = try JSONSerialization.data(withJSONObject: jsonData, options: .prettyPrinted)
@@ -41,26 +41,30 @@ class ViewController: UIViewController {
         }
         let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
             if error == nil {
-                if let responseReceived = response as? HTTPURLResponse {
-                    print(responseReceived)
-                }
+                guard let responseReceived = response as? HTTPURLResponse else {return}
+                print(responseReceived)
+            }
+            if error != nil {
+                print(error?.localizedDescription)
             }
         }
         task.resume()
-
     }
     
-    private func findNeedleIndex(haystack: NSArray, needle: String) -> Int{
-        for word in haystack {
-            if needle == word as? String {
-                return haystack.index(of: word)
+    private func returnArray(prefix: String, array: NSArray) -> NSMutableArray {
+        let mutableArray = NSMutableArray(array: array)
+        for word in mutableArray {
+            let wordStr = word as? String
+            if (wordStr?.contains(prefix))! {
+                mutableArray.remove(word)
             }
         }
-        return 0
+        print(mutableArray)
+        return mutableArray
     }
-    
-    private func receiveAPIInfo() {
-        let urlString = "http://challenge.code2040.org/api/haystack"
+
+    private func getDictionary() {
+        let urlString = "http://challenge.code2040.org/api/prefix"
         guard let url = URL(string: urlString) else {return}
         let session = URLSession.shared
         let request = NSMutableURLRequest(url: url)
@@ -76,26 +80,25 @@ class ViewController: UIViewController {
             print("there was an error parsing json")
         }
         
-        let task = session.dataTask(with: request as URLRequest) { [unowned self](data, response, error) in
+        let task = session.dataTask(with: request as URLRequest) { [unowned self] (data , response, error) in
             if error == nil {
                 guard let responseReceived = response as? HTTPURLResponse else {return}
                 print(responseReceived)
                 do {
-                    guard let dataReceived = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:AnyObject] else {return}
+                    guard let dataReceived = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary else {return}
                     print(dataReceived)
-                    guard let haystack = dataReceived["haystack"] as? NSArray else {return}
-                    print(haystack)
-                    guard let needle = dataReceived["needle"] as? String else {return}
-                    print(needle)
-                    let index = self.findNeedleIndex(haystack: haystack, needle: needle)
-                    self.postAPIData(location: index)
+                    guard let prefix = dataReceived["prefix"] as? String else {return}
+                    guard let array = dataReceived["array"] as? NSArray else {return}
+                    let arrayToPost = self.returnArray(prefix: prefix, array: array)
+                    self.postArray(array: arrayToPost)
+                    
                 } catch {
-                    print("conversion failed")
+                    print("there was an error parsing the json")
                 }
             }
         }
         task.resume()
     }
-
+    
 }
 
